7ed0c83c49f34cab6bb7e253e231c5dd076c4d79
Khetsophon Chausueb <wichanon.wic@gmail.com>
2026-06-14 10:37:00 +0700

    fix: successfully migrate project to ES Module and upgrade Node version

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,19 +1,12 @@
 import { defineConfig, devices } from '@playwright/test';
 
 /**
- * Read environment variables from file.
- * https://github.com/motdotla/dotenv
- */
-// import dotenv from 'dotenv';
-// import path from 'path';
-// dotenv.config({ path: path.resolve(__dirname, '.env') });
-
-/**
  * See https://playwright.dev/docs/test-configuration.
  */
 export default defineConfig({
   testDir: './tests',
-  testMatch: /.*\.spec\.(ts|mts)/, // ✨ เพิ่มบรรทัดนี้เพื่อให้ Playwright รันไฟล์ .mts ด้วย
+  /* สั่งให้รองรับทั้งไฟล์ .ts และ .mts */
+  testMatch: /.*\.spec\.(ts|mts)/, 
   /* Run tests in files in parallel */
   fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
@@ -26,10 +19,6 @@ export default defineConfig({
   reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
-    /* Base URL to use in actions like `await page.goto('')`. */
-    // baseURL: 'http://localhost:3000',
-
-    /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
   },
 
@@ -39,12 +28,10 @@ export default defineConfig({
       name: 'chromium',
       use: { ...devices['Desktop Chrome'] },
     },
-
     {
       name: 'firefox',
       use: { ...devices['Desktop Firefox'] },
     },
-
     {
       name: 'webkit',
       use: { ...devices['Desktop Safari'] },
